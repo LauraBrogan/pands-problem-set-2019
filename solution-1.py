15d9adbd0d354aa97b59d90bee17f4761b29eb88
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-09 16:42:36 +0000

    First attempt at solution 1

--- solution-1.py
+++ solution-1.py
@@ -1,1 +1,14 @@
-Solution to Problem 1
+# Solution to Problem 1
+# Input any positive integer and output the sum of all numbers between one and that numbers
+
+start = 10
+ans = 1
+
+i = 1
+
+while i > = start:
+    ans = ans + ans 
+
+
+print (ans)
+
