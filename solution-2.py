54ab09f19dcc8e9bd3ce2b8df834f7c2fca305e1
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-10 18:41:03 +0000

    Attempt 1 Solution 2

--- solution-2.py
+++ solution-2.py
@@ -1,1 +1,9 @@
-Solution to Problem 2
+# Solution to Problem 2
+# Is today a day that begins with the letter T 
+
+import datetime
+
+if datetime.today(). == [1] [3]
+  print("Yes today begins with a T.")
+else:
+  print("Unfortunately today is not a day that begins with T.")
