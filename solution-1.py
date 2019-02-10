18c11a2aa8e4c518a389a62156cd8a18fd974bee
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-10 12:06:45 +0000

    Fist attempt at Solution 1

--- solution-1.py
+++ solution-1.py
@@ -1,14 +1,9 @@
 # Solution to Problem 1
 # Input any positive integer and output the sum of all numbers between one and that numbers
 
-start = 10
-ans = 1
-
-i = 1
-
-while i > = start:
-    ans = ans + ans 
-
-
-print (ans)
+n = int(input("Input a number: "))
+  if n > 1:
+        print (n)   
+                else:
+                   print ("Unfortunately this is no a positive integer")
 
