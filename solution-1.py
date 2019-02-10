d10b6712da5f0db283cdc223f018f47f3d38dfa0
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-10 13:08:15 +0000

    Third attemp to Solution 1 inputting integer and adding 1 to it

--- solution-1.py
+++ solution-1.py
@@ -2,6 +2,13 @@
 # Input any positive integer and output the sum of all numbers between one and that numbers
 
 n = int(input("Input a number: "))
-if n > 1: print (n)
-if n < 1: print ("Unfortunately this is not a positive integer")
+# n is asking the use to input a postive number
 
+
+if n > 1: sum_num = n + 1 
+print (sum_num)
+    #if the user inputs a postive number the program runs and sums all of the number between one and that number and then displays it
+
+
+if n < 1: print ("Unfortunately this is not a positive integer")
+    #if the user inputs a negative number the programe displays "this is not a positive integer"
