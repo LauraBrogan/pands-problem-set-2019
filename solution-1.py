b59f9fba5ad64c0373f1ddc1fe5e640034d264f1
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-10 14:31:45 +0000

    Attempt 4 Solution 1

--- solution-1.py
+++ solution-1.py
@@ -2,13 +2,17 @@
 # Input any positive integer and output the sum of all numbers between one and that numbers
 
 n = int(input("Input a number: "))
-# n is asking the use to input a postive number
+# n is asking the user to input a postive number
 
+if n < 0: print ("Unfortunately this is not a positive integer") 
+quit () 
 
-if n > 1: sum_num = n + 1 
+elif n > 0:
+    pass sum_num = (n * (n + 1)) /2
+    
 print (sum_num)
-    #if the user inputs a postive number the program runs and sums all of the number between one and that number and then displays it
 
 
-if n < 1: print ("Unfortunately this is not a positive integer")
-    #if the user inputs a negative number the programe displays "this is not a positive integer"
+    #if the user inputs a negative number the programe displays "this is not a positive integer" and ends the program
+    #if the user inputs a postive number the program runs and sums all of the numbers between one and that number and then displays it
+
