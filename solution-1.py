81acd04a2f030bf08417bb802b35a095f6c08623
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-10 12:41:17 +0000

    Second Attempt at Solution 1 inputting positive integer

--- solution-1.py
+++ solution-1.py
@@ -2,8 +2,6 @@
 # Input any positive integer and output the sum of all numbers between one and that numbers
 
 n = int(input("Input a number: "))
-  if n > 1:
-        print (n)   
-                else:
-                   print ("Unfortunately this is no a positive integer")
+if n > 1: print (n)
+if n < 1: print ("Unfortunately this is not a positive integer")
 
