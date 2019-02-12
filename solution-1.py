83c5414539c4c6f5049d0ba6c1641e5f368af60b
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-12 16:07:19 +0000

    Attempt 5 Solution 1, Attempt 3 Solution 2

--- solution-1.py
+++ solution-1.py
@@ -4,13 +4,14 @@
 n = int(input("Input a number: "))
 # n is asking the user to input a postive number
 
-if n < 0: print ("Unfortunately this is not a positive integer") 
-quit () 
+if n < 0:
+     print("Unfortunately this is not a positive integer") 
+quit() 
 
-elif n > 0:
+if n > 0:
     pass sum_num = (n * (n + 1)) /2
     
-print (sum_num)
+    print(sum_num)
 
 
     #if the user inputs a negative number the programe displays "this is not a positive integer" and ends the program
--- solution-2.py
+++ solution-2.py
@@ -4,16 +4,20 @@
 import datetime
 
 if datetime.datetime.today().weekday() == 1:
- print("Yes today begins with a T.")
+    print("Yes today begins with a T.")
+if datetime.datetime.today().weekday() == 3:
+    print("Yes today begins with a T.")
 
 # If today is Tuesday please display to the user that yes today begins with a T. 
  
-if datetime.datetime.today().weekday() == 3:
-    print("Yes today begins with a T.")
 # If today is Thursday please display to the user that yes today begins with a T.
 
-else:
-    print("Unfortunately today is not a day that begins with T.")
+while datetime.datetime.today().weekday() == 0:
+    while datetime.datetime.today().weekday() == 2:
+        while datetime.datetime.today().weekday() == 5:
+            while datetime.datetime.today().weekday() == 6:
+                while datetime.datetime.today().weekday() == 7:
+                     print("Unfortunately today is not a day that begins with T.")
 # If today is not Tuesday or Thursday please display to the user that unfortunately today is not a day that begins with a T.
 
 # Laura Brogan 10/02/2019
