6849ac257beb831ce39433f7622a3bfd523911c7
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-12 16:43:13 +0000

    Attempt 6 Solution 1 else function causing difficulties

--- solution-1.py
+++ solution-1.py
@@ -1,17 +1,20 @@
 # Solution to Problem 1
 # Input any positive integer and output the sum of all numbers between one and that numbers
 
-n = int(input("Input a number: "))
+n = int(input("Input a Positive Number: "))
 # n is asking the user to input a postive number
 
 if n < 0:
      print("Unfortunately this is not a positive integer") 
 quit() 
 
-if n > 0:
-    pass sum_num = (n * (n + 1)) /2
-    
-    print(sum_num)
+
+sum = 0
+while (n>0):
+    sum += n
+    n-= 1
+
+print("This is the Sum", sum)
 
 
     #if the user inputs a negative number the programe displays "this is not a positive integer" and ends the program
