8445a46ac71436300fcfad38ef0a18bb047f6fdc
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-13 10:27:25 +0000

    Attempt 5 Solution 2

--- solution-2.py
+++ solution-2.py
@@ -6,30 +6,14 @@ import datetime
 
 if datetime.datetime.today().weekday() == 1:
     print("Yes today begins with a T.")
+    
 if datetime.datetime.today().weekday() == 3:
     print("Yes today begins with a T.")
 
-# If today is Tuesday please display to the user that yes today begins with a T. 
+# If today is Tuesday  or a Thursday please display to the user that yes today begins with a T. 
  
-# If today is Thursday please display to the user that yes today begins with a T.
-
-if datetime.datetime.today().weekday() == 0:
-                        print("Unfortunately today is not a day that begins with T.")
-
-if datetime.datetime.today().weekday() == 2:
-                        print("Unfortunately today is not a day that begins with T.")
-
-if datetime.datetime.today().weekday() == 4:
-                        print("Unfortunately today is not a day that begins with T.")
-
-if datetime.datetime.today().weekday() == 5:
-                        print("Unfortunately today is not a day that begins with T.")
-
-if datetime.datetime.today().weekday() == 6:
-                        print("Unfortunately today is not a day that begins with T.")
-
-if datetime.datetime.today().weekday() == 7:
-                        print("Unfortunately today is not a day that begins with T.")
+else:
+    print("Unfortunately today is not a day that begins with T.")
 
 # If today is not Tuesday or Thursday please display to the user that unfortunately today is not a day that begins with a T.
 
