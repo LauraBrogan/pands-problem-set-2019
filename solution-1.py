05d2dfbc42071437f7b3bbb51fb0dabe7c97eeb9
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-02-19 19:46:01 +0000

    Attemp 7 Solution 2

--- solution-1.py
+++ solution-1.py
@@ -4,20 +4,19 @@
 n = int(input("Input a Positive Number: "))
 # n is asking the user to input a postive number
 
-total = 0
+total = 0 
 
-if n < 0:
-     print("Unfortunately this is not a positive integer") 
-quit() 
-
+if n <= 0:
+    print("Unfortunately this is not a positive integer.") 
+    quit()
+# If the user inputs a negative number of a zero the programe displays "this is not a positive integer" and ends the program
+    
 while n > 0:
     total = total + n
     n = n - 1
-   
-print(total)
+print(total,"This is the sum of all the numbers between one and the interger inputted above.")
 
+# If the user inputs a postive number the program runs and sums all of the numbers between one and that number and then displays the total.
 
-
-    #if the user inputs a negative number the programe displays "this is not a positive integer" and ends the program
-    #if the user inputs a postive number the program runs and sums all of the numbers between one and that number and then displays it
+# Laura Brogan 19/02/2019
 
