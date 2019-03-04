e2ea6eed92490f5cfff199d9f588ecb1c9fb21ed
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-04 16:25:06 +0000

    Attempt 1 Solution 8

--- solution-8.py
+++ solution-8.py
@@ -1,1 +1,13 @@
-Solution to Problem 8
+# Solution to Problem 8
+# Program outputs today's date and time in the format "Monday, January 10th 2019 at 1:15pm"
+
+# To start we import the Python datetime module.
+import datetime
+
+now = datetime.datetime.now()
+
+print("Todays Date and time:")
+print(now.strftime("%A,%B,%d,%h,%M,%I,%p"))
+# Used lecture from week 6 as a base for the problem also looked at the Python tutorial.
+
+# Laura Brogan 19/02/2019
