4bc1aabc8f54c3f9e13e0e6ada7bffd8ca599452
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-04 16:34:39 +0000

    Attempt 2 Solution 8

--- solution-8.py
+++ solution-8.py
@@ -7,7 +7,7 @@ import datetime
 now = datetime.datetime.now()
 
 print("Todays Date and time:")
-print(now.strftime("%A,%B,%d,%h,%M,%I,%p"))
+print(now.strftime("%A,%B %Y at %I:%M %p"))
 # Used lecture from week 6 as a base for the problem also looked at the Python tutorial.
 
-# Laura Brogan 19/02/2019
+# Laura Brogan 
