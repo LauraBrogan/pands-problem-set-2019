0743152b13b7fe28d9ba9b5cb733de2850f83212
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-04 20:29:18 +0000

    Attempt 2 Solution 3

--- solution-3.py
+++ solution-3.py
@@ -2,9 +2,25 @@
 # Print all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
 
 
-lower=1000
-upper=10000
-n=6
-for i in range(lower,upper):
-    if (i%n==0):
-        print(i)
+#lower=1000
+#upper=10000
+#n=6
+#for i in range(lower,upper):
+   # if (i%n==0):
+      #  print(i)
+      #  and 
+
+
+
+nl=[]
+for x in range(1000, 10000):
+ if (x%6==0):
+        nl.append(str(x))
+print (','.join(nl))
+     
+
+
+
+#Reference: https://www.w3resource.com/python-exercises/python-conditional-exercise-1.php
+
+# Laura Brogan
