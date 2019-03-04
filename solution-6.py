e22b94b8c8d21f5459a07563ff8269ef947e7ecc
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-04 20:12:15 +0000

    Attempt 1 Solution 6

--- solution-6.py
+++ solution-6.py
@@ -1,1 +1,15 @@
-Solution to Problem 6
+# Solution to Problem 6
+# Ask the user to imput a sentence and output every second word.
+
+
+n = (input("Please enter a sentence: "))
+#order using positional argument
+print(positional_order = "{1},{2}".format('n'))
+print('\n--- Positional Order ---')
+print(positional_order)
+
+
+
+
+# Reference: https://www.programiz.com/python-programming/string
+# Laura Brogan 
