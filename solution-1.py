98e6b49762953f00788c5ea5ee54e46940ae5838
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-04 15:50:47 +0000

    Attempt 8 Solution 1

--- solution-1.py
+++ solution-1.py
@@ -1,22 +1,23 @@
 # Solution to Problem 1
-# Input any positive integer and output the sum of all numbers between one and that numbers
+# Ask the user to Input any positive integer and output the sum of all numbers between one and that number.
 
-n = int(input("Input a Positive Number: "))
+
 # n is asking the user to input a postive number
+n = int(input("Input a Positive Number: "))
 
+# If the user inputs a negative number or a zero the programe displays "this is not a positive integer" and ends the program
 total = 0 
 
 if n <= 0:
     print("Unfortunately this is not a positive integer.") 
     quit()
-# If the user inputs a negative number of a zero the programe displays "this is not a positive integer" and ends the program
-    
+
+# If the user inputs a postive number the program runs and sums all of the numbers between one and that number and then displays the total.   
 while n > 0:
     total = total + n
     n = n - 1
 print(total,"This is the sum of all the numbers between one and the interger inputted above.")
 
-# If the user inputs a postive number the program runs and sums all of the numbers between one and that number and then displays the total.
-
+# Used Class Tutorial for Factorial Problem as base for creation of the above. 
 # Laura Brogan 19/02/2019
 
