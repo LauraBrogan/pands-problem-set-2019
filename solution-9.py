5859552ec6b56fbbcb20b9349fd78258f06eded5
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-11 21:54:13 +0000

    Attempt 6 Solution 9

--- solution-9.py
+++ solution-9.py
@@ -1,17 +1,18 @@
 # Solution to Problem 9
 # Program to read a text file and output every second line. 
 
-"Please open text file:      "
+userinput = input("Please open text file:") 
 
 # The the "With" statement, simplifies the handling of opening text file "Staunton" and will automatically close the file.
 # We open file as F and count for every line in F, if the line is divisible by 2 we print the line. 
 
-with open('Staunton.txt','r') as f:
+with open('userinput','r') as f:
     count = 0
+    # process line
     for line in f:
-       count+=1
-       if count % 2 == 0:
-        print(line)
+      count+=1
+    if count % 2 == 0:
+       print(Line)
 
 
 
