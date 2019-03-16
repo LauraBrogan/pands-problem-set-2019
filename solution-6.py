2a497d779a6af8fa37c92a9a0dbc49a88c677c90
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-16 15:34:27 +0000

    Attempt 3 Solution6

--- solution-6.py
+++ solution-6.py
@@ -2,15 +2,29 @@
 # Ask the user to imput a sentence and output every second word.
 
 
-n = (input("Please enter a sentence: "))
+string = (input("Please enter a sentence: "))
 #order using positional argument
+even_words = string.split('string')[::2]
+print(even_words)
 
-count = 0
+
+
+
+
+
+#count = 0
     # process line
-for line in n:
- count+=1
-if count % 2 == 0:
-    print(n)
+#for word in n:
+ #count+=1
+#if count % 2 == 0:
 
+
+    
+
+#string = n
+#even_words = string.split('input')[::2]
+#print(n)
+
+# Reference: https://stackoverflow.com/questions/54857129/write-a-program-that-takes-a-user-input-string-and-outputs-every-second-word
 # Reference: https://www.programiz.com/python-programming/string
 # Laura Brogan 
