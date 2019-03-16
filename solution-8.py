4519661a16ad1d395af073864c3bc15a1ac7e52c
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-16 16:08:14 +0000

    Attempt 6 Solution 8 Trying to get th on end of date

--- solution-8.py
+++ solution-8.py
@@ -2,16 +2,25 @@
 # Program outputs today's date and time in the format "Monday, January 10th 2019 at 1:15pm"
 
 # To start we import the Python datetime module.
-import datetime
+import datetime 
 
 now = datetime.datetime.now()
+day = today.day
+#date_string = today.strftime('%A, %B %# %Y at %I:%M %p')
+
 
 print("Todays Date and time:")
+if (3 < day < 21) or (23 < day < 31):
+  day = str(day) + 'th'
+else:
+  suffixes = {1: 'st', 2: 'nd', 3: 'rd'}
+  day = str(day) + suffixes[day % 10]
 
-print(now.strftime("%A, %B %d %Y at %I:%M %p"))
+#print(date_string.replace('#', day), end='')
+print(now.strftime("%A, %B %d# %Y at %I:%M %p"))
 
 
-
+# Reference Verbatim https://www.robjwells.com/2013/10/date-suffixes-in-python/
 # Reference:https://www.saltycrane.com/blog/2008/06/how-to-get-current-date-and-time-in/
 # Used lecture from week 6 as a base for the problem also looked at the Python tutorial.
 # Laura Brogan 
