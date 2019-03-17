3619034fadc65c1cf546d5316431900a4b8a28ab
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-17 15:04:56 +0000

    Attempt 3 Solution 4

--- solution-4.py
+++ solution-4.py
@@ -2,43 +2,27 @@
 # 
 
 # n is asking the user to input a postive integer.
-i = int(input("Input a Positive Number: "))
+n = int(input("Input a Positive Number: "))
 
 # If the user inputs a negative number or a zero the programe displays "this is not a positive number" and ends the program.
-if i <= 0:
+if n <= 0:
     print("Unfortunately this is not a positive number.") 
     quit()
 
-# If the user inputs a postive number greater than zero the program runs.   
-if i > 0:
-    while i !=1:
-     def collatz(i):
-        if i % 2 == 0:
-         print(i // 2)
-         return i // 2
-
-        elif i % 2 == 1:
-         result = 3 * i + 1
-         print(result)
-       
-
-
-
-
-      #  while i != 1:
-          # if i % 2==0:
-           #  i = (i//2)
-            #print(number)
-           # return (print(int(i)))
-
-           # while i % 2==1:
-          #   i = (3*i+1) 
-            #print(number)
-           # return (print(int(i)))
-quit()  
-        #continue
-
-
+   
 # 
 # Reference 
 # Laura Brogan 
+
+
+def collatz(n):
+    print(n)
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+            print(n)
+        else:
+            n = n * 3 + 1
+            print(n)
+collatz(n, end' ')
+
