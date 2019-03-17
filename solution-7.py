3ea3335b26bc3b590e73d9d4ab19b7bf851fc97a
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-17 12:37:05 +0000

    Solution 7 complete 17/03/2019

--- solution-7.py
+++ solution-7.py
@@ -1,24 +1,26 @@
 # Solution to Problem 7
 # Ask the user to Input any positive floating point number and output an approximation of its square root.
+# To start I import the Python math module
 import math
 
 # n is asking the user to input a postive floating point number
 n = float(input("Input a Positive Floating Point Number: "))
 
-# If the user inputs a negative number or a zero the programe displays "this is not a positive number" and ends the program
-total = 0.0 
-
+# If the user inputs a negative number or a zero the programe displays "this is not a positive number" and ends the program.
 if n <= 0.0:
     print("Unfortunately this is not a positive number.") 
     quit()
 
-# If the user inputs a postive number the program runs and sums all of the numbers between one and that number and then displays the total.   
+# If the user inputs a postive number greater than zero the program runs.   
 while n > 0.0:
-#Verbatium lines 18-20 inc from reference 1 below
+# Verbatium lines 18-22 inc from reference 1 below.
+# n is the number the user inputs as a floating point number.
     n = float(n)
+# The built ini function calculates the square root of the inputted number.
     number_sqrt = n ** 0.5
-    #Displays the number we are finding the root of with 2 decimal place the square root with 5 decimal places.
-    print("Square Root of Floating point number %0.2f is %0.5f" %(n, number_sqrt))
+#Displays the number we are finding the root of with 1 decimal place the square root with 1 decimal place.
+    print("Square Root of Floating point number %0.1f is approx %0.1f" %(n, number_sqrt))
+# Closes the program
     quit()
 
 # Used solution 1 to 2019 problem set as the base for this answer.
