c6dfd32eb29bba666bcada2551207570deb57bb5
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-19 21:01:27 +0000

    Attempt 9 Solution 8 Working on the th

--- solution-8.py
+++ solution-8.py
@@ -1,39 +1,25 @@
 # Solution to Problem 8
 # Program outputs today's date and time in the format "Monday, January 10th 2019 at 1:15pm"
-
-from datetime import datetime as dt
-
-def suffix(d):
-    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
-
-def custom_strftime(format, t):
-    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
-
-print(custom_strftime('%B {S}, %Y', dt.now())
-
-
-
-
-
-
-
-
-
 # To start we import the Python datetime module.
 
 
-#from datetime import datetime as dt
-#now = dt.now()
+from datetime import datetime as dt
+import time
+now = dt.now()
 #def suffix(d):
     # return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+t = time.localtime()
+suffix = 'st' if t.tm_mday in [1,21,31] else 'nd' if t.tm_mday in [2, 22] else 'rd' if t.tm_mday in [3, 23] else 'th'
+
 
 #def custom_strftime(format, t):
    # return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
-#print("Todays Date and time:")
+print("Todays Date and time:")
+print(time.strftime('%d%%s %B %Y', t) % suffix)
 #print custom_strftime ('%B %d, %Y', dt.now())
 #print (custom_strftime("%A, %B {S} %Y atdt.now() %I:%M %p"))
 
-#print(now.strftime("%A, %B %d %Y at %I:%M %p"))
+#print(time.strftime("%A, %B %d %Y at %I:%M %p"))
 
 
 # Reference:https://www.saltycrane.com/blog/2008/06/how-to-get-current-date-and-time-in/
