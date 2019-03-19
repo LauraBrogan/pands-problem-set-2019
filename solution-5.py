0a1756fc837a30b50e8e01a92713d121834199f8
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-19 22:19:55 +0000

    Attempt 2 Solution 5

--- solution-5.py
+++ solution-5.py
@@ -1,11 +1,10 @@
 # Solution to Problem 5
-# Ask the user to Input a positive integer and output tell is user whether or not the number is a prime number.
+# Ask the user to Input a positive integer and output to the user whether or not the number is a prime number.
 
-
-# y is asking the user to input a postive integer number
+# y is asking the user to input a postive integer.
 y = int(input("Please enter a positive integer: "))
-# take input from the user
-if y <= 1:
+# take input from the user if it is greather than or 
+if y < 1:
     print("Unfortunately this is not a positive integer.") 
     quit()
 
