d4968782251b8b7f1bd3069e04586029726c2c6d
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-19 20:44:32 +0000

    Attemp 8 Solution 8 Trying to get th on end of date

--- solution-8.py
+++ solution-8.py
@@ -1,17 +1,40 @@
 # Solution to Problem 8
 # Program outputs today's date and time in the format "Monday, January 10th 2019 at 1:15pm"
 
+from datetime import datetime as dt
+
+def suffix(d):
+    return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
+
+def custom_strftime(format, t):
+    return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+
+print(custom_strftime('%B {S}, %Y', dt.now())
+
+
+
+
+
+
+
+
+
 # To start we import the Python datetime module.
-import datetime 
-
-now = datetime.datetime.now()
 
 
-print("Todays Date and time:")
+#from datetime import datetime as dt
+#now = dt.now()
+#def suffix(d):
+    # return 'th' if 11<=d<=13 else {1:'st',2:'nd',3:'rd'}.get(d%10, 'th')
 
-print(now.strftime("%A, %B %d %Y at %I:%M %p"))
+#def custom_strftime(format, t):
+   # return t.strftime(format).replace('{S}', str(t.day) + suffix(t.day))
+#print("Todays Date and time:")
+#print custom_strftime ('%B %d, %Y', dt.now())
+#print (custom_strftime("%A, %B {S} %Y atdt.now() %I:%M %p"))
+
+#print(now.strftime("%A, %B %d %Y at %I:%M %p"))
 
 
 # Reference:https://www.saltycrane.com/blog/2008/06/how-to-get-current-date-and-time-in/
 # Used lecture from week 6 as a base for the problem also looked at the Python tutorial.
-# Laura Brogan 
