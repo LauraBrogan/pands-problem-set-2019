27921a6dd046ada21c9eacc2119ae9d1e6ca408b
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-24 17:39:03 +0000

    Attempt 2 Solution 10 mathematics on strongest still

--- solution-10.py
+++ solution-10.py
@@ -1,15 +1,18 @@
 # Solution to Problem 10
 
 import matplotlib.pyplot as pl
+#lines = pl.plot(x, x^2, 2^x)
 x = 2
-lines = pl.plot(x, x^2, 2^x)
+#y1 = x
 pl.plot([0,4])
-pl.setp(lines, color='b', linewidth=4.0)
-pl.show()
-
+#pl.setp(lines'color','g', 'linewidth', 4.0)
 pl.ylabel('Plot of functions')
+#y2 = x^2
+y3 = 2^x
 
 
+
+pl.show()
 
 
 # use keyword args
@@ -17,3 +20,4 @@ pl.ylabel('Plot of functions')
 # or MATLAB style string value pairs
 #plt.setp(lines, 'color', 'r', 'linewidth', 2.0)
 #https://matplotlib.org/1.4.2/users/pyplot_tutorial.html
+#https://www.oreilly.com/library/view/python-data-science/9781491912126/ch01.html#help-and-documentation-in-ipython
