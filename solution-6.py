9ee4c6dd2994616916c0d92f3110783f34ed469c
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-25 11:44:10 +0000

    Attempt 8 Solution 6 Trying to get display of every second word without commas and brackets.

--- solution-6.py
+++ solution-6.py
@@ -1,32 +1,12 @@
 # Solution to Problem 6
 # Ask the user to imput a sentence and output every second word.
 
-#user = input("Please Enter a Sentence:")
-#even_words = user.split[::2]
-#print(even_words)
+user = input("Please Enter a Sentence:")
+even_words = user.split(' ')
+print(even_words[::2], end ="")
+
 
 
 # Reference: https://stackoverflow.com/questions/54857129/write-a-program-that-takes-a-user-input-string-and-outputs-every-second-word
 # Reference: https://www.programiz.com/python-programming/string
 # Laura Brogan 
-
-sentenceInput=(input ("Please enter sentence: "))
-
-# Function for deleting every 2nd word
-def wordDelete(sentence):
-
-    # Splitting sentence into pieces by thinking they're seperated by space.
-    # Comma and other signs are kept.
-    sentenceList = sentence.split(" ")
-
-    # Checking if sentence contains more than 1 word seperated and only then remove the word
-  #  if len(sentenceList) > 1:
-    sentenceList.remove(sentenceList[::2])
-
-    # Re-joining sentence
-    droppedSentence = ' '.join(sentenceList)
-
-    # Returning the result
-    return droppedSentence
-
-    print(wordDelete(sentenceInput))
