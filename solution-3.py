28cdac0f8cd87dfdb492d43c589da587a34cbdd8
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-25 10:46:33 +0000

    Solution 3 Completed 25/03/2019

--- solution-3.py
+++ solution-3.py
@@ -1,13 +1,22 @@
 # Solution to Problem 3
 # Print all numbers between 1,000 and 10,000 that are divisible by 6 but not 12.
 
+# Identify Lower and upper limits. Lower = 1000, Upper = 10000.
 lower=1000
 upper=10000
+
+# Idnetify the divisors, n=6 and x=12.
 n=6
 x=12
-for i in range(lower,upper+1):
-     if(i%n==0) and (i%12!=0):
-       print(i)     
-#https://www.sanfoundry.com/python-program-print-numbers-range-divisible-given-number/
 
-	
+# For every number represneted by i in the range from lower limit to upper limit.
+for i in range(lower,upper+1):
+# If i the current number is divisible by n (6) and not divisible by x (12) 
+     if(i%n==0) and (i%x!=0):
+# Print the number to the user.   
+       print (i)  
+
+# Reference: https://www.sanfoundry.com/python-program-print-numbers-range-divisible-given-number/
+# Reference: https://www.w3resource.com/python-exercises/python-conditional-exercise-1.php
+# Reference: Week 4 Loops https://web.microsoftstream.com/video/20963781-2aea-4302-ba73-5b8f327f4189
+# Laura Brogan 	25/03/2019
