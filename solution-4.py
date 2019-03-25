bfbdd339a1dd3d2dd4ce809755d81f0231a4e558
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2019-03-25 09:19:10 +0000

    Solution 4 Complete 25/03/19

--- solution-4.py
+++ solution-4.py
@@ -1,5 +1,8 @@
 # Solution to Problem 4
-# 
+# Ask user to input any positive integer and outpts the successive valuses of the following calculation. 
+# At each step calcuate the next value by taking the current value and, it is is even, divide it by two.
+# But is if is odd, multiply it by thhree and add one.
+# End program if the current value is one. 
 
 # n is asking the user to input a postive integer.
 n = int(input("Input a Positive Integer: "))
@@ -8,17 +11,21 @@ n = int(input("Input a Positive Integer: "))
 if n <= 0:
     print("Unfortunately this is not a positive Integer.") 
     quit()
-
+# The Collatz conjecture is a conjecture that a particular sequence always reaches 1. 
+# The sequence is defined as: start with a number n. 
+# Verbatium from her to the end https://stackoverflow.com/q/33324432.
 def collatz(n):
     print(n)
-    #this will stop the program when it gets to one.
+#This will stop the program when it gets to one.
     while n != 1:
+# If The next number in the sequence is even n is divided by 2 and print that number.
         if n % 2 == 0:
             n = n // 2
             print(n)
+# If n is odd we triple n and add  1 and print that number
         else:
             n = n * 3 + 1
             print(n)
-collatz (n)
+collatz(n)
 # Reference https://stackoverflow.com/q/33324432
-# Laura Brogan 
+# Laura Brogan 25/03/19
